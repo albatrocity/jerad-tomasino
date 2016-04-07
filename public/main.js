5f231dd6f39a3a2669e3361cd6b71938e09340cf
Ross Brown <ross@cremalab.com>
2016-04-06 22:50:42 -0500

    initial

---
+++ public/main.js
@@ -0,0 +1,9 @@
+//
+// var s = Snap("#svg");
+//
+// Snap.load("/images/tvbars_vector.svg", function (f) {
+//   // Making croc draggable. Go ahead drag it around!
+//   g.drag();
+//   // Obviously drag could take event handlers too
+//   // Looks like our croc is made from more than one polygon...
+// });
---
+++ server.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+
+let goodOptions = {
+  reporters: [{
+    reporter: require('good-console'),
+    events: { log: ['error'], response: '*' }
+  }]
+}
+
+server.register([{
+  register: require('good'),
+  options: goodOptions
+},
+  require('inert')
+], (err) => {
+  // Add the route
+  server.route({
+    method: 'GET',
+    path:'/',
+    handler: function (request, reply) {
+      return reply.file('./public/index.html');
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: 'public',
+        listing: true
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{filename}',
+    handler: {
+      file: function (request) {
+        return `./public/${request.params.filename}`;
+      }
+    }
+  });
+
+  // Start the server
+  server.start((err) => {
+
+    if (err) {
+      throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+  });
+
+})
