4c9a9ef5b6d695b597e1f3eea19431855522371e
Ross Brown <ross@cremalab.com>
2016-05-31 23:55:46 -0500

    extra text on hover

--- public/main.js
+++ public/main.js
@@ -1,9 +1,16 @@
-//
-// var s = Snap("#svg");
-//
-// Snap.load("/images/tvbars_vector.svg", function (f) {
-//   // Making croc draggable. Go ahead drag it around!
-//   g.drag();
-//   // Obviously drag could take event handlers too
-//   // Looks like our croc is made from more than one polygon...
-// });
+document.addEventListener('DOMContentLoaded', function() {
+  // var svgEl = document.getElementById('bars')
+  var s = Snap('#bars');
+  // console.log(s);
+
+  Snap.load("/images/tvbars_vector.svg", function (f) {
+    // Making croc draggable. Go ahead drag it around!
+    var doc = f.select('#TVBars');
+    console.log(doc);
+    s.append(doc);
+    // document.querySelector('#main-header').appendChild(doc)
+    // Obviously drag could take event handlers too
+    // Looks like our croc is made from more than one polygon...
+  });
+
+}, false);
